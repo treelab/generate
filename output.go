766fca9852683cf8e6b84f2b91c12772acfccaff
SolaTyolo <29033393+SolaTyolo@users.noreply.github.com>
2024-03-27 10:48:54 +0800

    fix: 移除marshal的方法

--- output.go
+++ output.go
@@ -60,7 +60,7 @@ func Output(w io.Writer, g *Generator, pkg string) {
 	for _, k := range getOrderedStructNames(structs) {
 		s := structs[k]
 		if s.GenerateCode {
-			emitMarshalCode(codeBuf, s, imports)
+			// emitMarshalCode(codeBuf, s, imports)
 			emitUnmarshalCode(codeBuf, s, imports)
 		}
 	}
